05f06b891a3ec4102cd40457a4fc58eae585e61c
laris <larissasouza201565@gmail.com>
2024-02-26 10:48:55 -0300

    add tela editar livros

---
+++ app/src/main/java/com/room/bookflow/activities/EditBookActivity.java
@@ -0,0 +1,15 @@
+package com.room.bookflow.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.room.bookflow.R;
+
+public class EditBookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_book);
+    }
+}
