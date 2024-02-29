666b988281bf63dac4f627a0489f469c97ca231a
laris <larissasouza201565@gmail.com>
2024-02-29 13:37:12 -0300

    add tela info proprietario

---
+++ app/src/main/java/com/room/bookflow/activities/OwnerScreenActivity.java
@@ -0,0 +1,16 @@
+package com.room.bookflow.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.room.bookflow.R;
+
+public class OwnerScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_owner_screen);
+    }
+}
