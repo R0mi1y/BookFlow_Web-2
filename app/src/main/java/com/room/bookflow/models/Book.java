f6537aaca5c34da1a7e26a27bd428d9672364a41
laris <larissasouza201565@gmail.com>
2024-03-02 19:01:31 -0300

    correcao de bug

--- app/src/main/java/com/room/bookflow/models/Book.java
+++ app/src/main/java/com/room/bookflow/models/Book.java
@@ -45,6 +45,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
@@ -78,6 +79,7 @@ public class Book {
     private double rating;
     private boolean availability;
     private int ownerId = -1;
+    
 
     public Book() {
     }
