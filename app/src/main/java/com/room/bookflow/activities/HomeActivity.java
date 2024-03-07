764d31af6447c7305cf527cf61c0d474608247f7
nevesfg <jaojose711@gmail.com>
2024-03-07 16:42:56 -0300

    Add button NAV SCANQR

--- app/src/main/java/com/room/bookflow/activities/HomeActivity.java
+++ app/src/main/java/com/room/bookflow/activities/HomeActivity.java
@@ -126,6 +126,9 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         }else if(itemSelecionado == R.id.notifications){
             Intent intent = new Intent(HomeActivity.this, NotificationsActivity.class);
             startActivity(intent);
+        }else if(itemSelecionado == R.id.scannerqr){
+            Intent intent = new Intent(HomeActivity.this, QRCodeScannerActivity.class);
+            startActivity(intent);
         }else if(itemSelecionado == R.id.loan){
             Intent intent = new Intent(HomeActivity.this, ListBooksActivity.class);
             intent.putExtra("filter", "MY_BOOKS");
